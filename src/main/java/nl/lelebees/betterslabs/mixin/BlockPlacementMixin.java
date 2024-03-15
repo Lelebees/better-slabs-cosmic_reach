573e9c958197364686b04bce579a37d994d0fc25
Lelebees <Lelebees@protonmail.com>
2024-03-15 10:15:18 +0100

    Start of horizontal slabbing

--- src/main/java/nl/lelebees/betterslabs/mixin/BlockPlacementMixin.java
+++ src/main/java/nl/lelebees/betterslabs/mixin/BlockPlacementMixin.java
@@ -1,6 +1,8 @@
 package nl.lelebees.betterslabs.mixin;
 
 
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.collision.BoundingBox;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import finalforeach.cosmicreach.gamestates.InGame;
@@ -18,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BlockPlacementMixin {
 
     @Inject(method = "placeBlock", at = @At("HEAD"))
-    private void injected(World world, BlockState targetBlockState, BlockPosition blockPos, double timeSinceLastInteract, CallbackInfo ci, @Local LocalRef<BlockState> blockStateLocalRef) {
+    private void verticalSlabs(World world, BlockState targetBlockState, BlockPosition blockPos, double timeSinceLastInteract, CallbackInfo ci, @Local(argsOnly = true) LocalRef<BlockState> blockStateLocalRef) {
         BlockState blockState = blockStateLocalRef.get();
         if (!blockState.stringId.contains("type=vertical")) {
             return;
@@ -38,5 +40,15 @@ public class BlockPlacementMixin {
         String saveKey = blockState.getBlockId() + "[" + stateIdBuilder + "]";
         blockState = BlockState.getInstance(saveKey);
         blockStateLocalRef.set(blockState);
+    }
+
+    @Inject(method = "raycast", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/world/BlockSelection;placeBlock(Lfinalforeach/cosmicreach/world/World;Lfinalforeach/cosmicreach/world/blocks/BlockState;Lfinalforeach/cosmicreach/world/BlockPosition;D)V"))
+    private void horizontalSlabs(World world, Camera worldCamera, CallbackInfo ci, @Local(name = "breakingBlockPos") BlockPosition highlightedBlockPosition) {
+        /* TODO: We'll need to look at the highlightedBlockPosition, and its Block.
+        We want to determine what face the player is (was) looking at, and if the face was a side face, what half of the face.
+        If the face is bottom or top, place the opposite slab (bottom face > top slab, top face > bottom slab)
+        If the face is side, place the slab corresponding to the half of the face the player is looking at
+        (top half > top slab, bottom half > bottom slab) */
+        BoundingBox boundingBox = highlightedBlockPosition.getBlockState().getModel().boundingBox;
     }
 }
