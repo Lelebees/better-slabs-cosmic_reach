7be7fd21b42f3736360dbc096424d63584299794
Lelebees <Lelebees@protonmail.com>
2024-03-15 20:14:40 +0100

    IT'S WORKING!
    who knew that a simple @WrapOperation is all a man needs to fix his raycast struggles?

--- src/main/java/nl/lelebees/betterslabs/mixin/BlockPlacementMixin.java
+++ src/main/java/nl/lelebees/betterslabs/mixin/BlockPlacementMixin.java
@@ -1,7 +1,8 @@
 package nl.lelebees.betterslabs.mixin;
 
 
-import com.badlogic.gdx.graphics.Camera;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import finalforeach.cosmicreach.gamestates.InGame;
@@ -11,6 +12,7 @@ import finalforeach.cosmicreach.world.World;
 import finalforeach.cosmicreach.world.blocks.BlockState;
 import nl.lelebees.betterslabs.extras.ViewDirection;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,9 +23,13 @@ import static nl.lelebees.betterslabs.extras.LeleUtil.fetchNewState;
 @Mixin(BlockSelection.class)
 public class BlockPlacementMixin {
 
+    @Shadow
+    private double timeSinceBlockModify;
+
     @Inject(method = "placeBlock", at = @At("HEAD"))
     private void verticalSlabs(World world, BlockState targetBlockState, BlockPosition blockPos, double timeSinceLastInteract, CallbackInfo ci, @Local(argsOnly = true) LocalRef<BlockState> blockStateLocalRef) {
         BlockState blockState = blockStateLocalRef.get();
+        System.out.println("boutta place a block: " + blockState.getSaveKey());
         if (!blockState.stringId.contains("type=vertical")) {
             return;
         }
@@ -33,25 +39,19 @@ public class BlockPlacementMixin {
     }
 
 
-    @Inject(method = "raycast", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/world/BlockSelection;placeBlock(Lfinalforeach/cosmicreach/world/World;Lfinalforeach/cosmicreach/world/blocks/BlockState;Lfinalforeach/cosmicreach/world/BlockPosition;D)V"))
-    private void horizontalSlabs(World world, Camera worldCamera, CallbackInfo ci, @Local(name = "breakingBlockPos") BlockPosition selectedBlockPosition, @Local(name = "placingBlockPos") BlockPosition placingBlockPosition, @Local(name = "targetBlockState") LocalRef<BlockState> targetBlockStateRef) {
-        /* TODO: We'll need to look at the selectedBlockPosition, and its Block.
-        We want to determine what face the player is (was) looking at, and if the face was a side face, what half of the face.
-        If the face is bottom or top, place the opposite slab (bottom face > top slab, top face > bottom slab)
-        If the face is side, place the slab corresponding to the half of the face the player is looking at
-        (top half > top slab, bottom half > bottom slab) */
-        BlockState targetBlockState = targetBlockStateRef.get();
+    @WrapOperation(method = "raycast", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/world/BlockSelection;placeBlock(Lfinalforeach/cosmicreach/world/World;Lfinalforeach/cosmicreach/world/blocks/BlockState;Lfinalforeach/cosmicreach/world/BlockPosition;D)V"))
+    private void horizontalSlabs(BlockSelection instance, World world, BlockState targetBlockState, BlockPosition targetBlockPos, double blockPos, Operation<Void> original, @Local(name = "breakingBlockPos") BlockPosition selectedBlockPosition, @Local(name = "targetBlockState") LocalRef<BlockState> targetBlockStateRef) {
+        // TODO: If the face is side, place the slab corresponding to the half of the face the player is looking at (top half > top slab, bottom half > bottom slab)
         if (!targetBlockState.stringId.contains("type=bottom") && !targetBlockState.stringId.contains("type=top")) {
+            original.call(instance, world, targetBlockState, targetBlockPos, timeSinceBlockModify);
             return;
         }
-        int deltaY = placingBlockPosition.getGlobalY() - selectedBlockPosition.getGlobalY();
+        int deltaY = targetBlockPos.getGlobalY() - selectedBlockPosition.getGlobalY();
 
         String[] blockStateId = targetBlockState.stringId.split("=");
         String orientation = blockStateId[blockStateId.length - 1];
-        System.out.println("old orientation: " + orientation);
         orientation = determineOrientation(deltaY, orientation);
-        System.out.println("new orientation:" + orientation);
-        targetBlockStateRef.set(fetchNewState(targetBlockState, orientation));
+        original.call(instance, world, fetchNewState(targetBlockState, orientation), targetBlockPos, timeSinceBlockModify);
     }
 
     @Unique
@@ -60,12 +60,9 @@ public class BlockPlacementMixin {
             System.out.println("side!");
             return originalOrientation;
         }
-        System.out.println("Top/bottom");
         if (deltaY < 0) {
-            System.out.println("bottom!");
             return "top";
         }
-        System.out.println("top!");
         return "bottom";
     }
 }
